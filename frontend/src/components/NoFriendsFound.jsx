c3f53f1258b8d6b3bf23c484544db3756f00270d
itsrahul_8 <149181581+rahuldk1105@users.noreply.github.com>
2025-05-06 19:18:52 +0530

    first commit

---
+++ frontend/src/components/NoFriendsFound.jsx
@@ -0,0 +1,13 @@
+const NoFriendsFound = () => {
+  return (
+    <div className="card bg-base-200 p-6 text-center">
+      <h3 className="font-semibold text-lg mb-2">No friends yet</h3>
+      <p className="text-base-content opacity-70">
+        Calmly wait until your friends open their account and display in your reccomendation soon
+        
+      </p>
+    </div>
+  );
+};
+
+export default NoFriendsFound;
